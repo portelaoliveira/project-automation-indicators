4f606a3891378533770e1555eee76a209349a6bf
Danilo Portela <daniloportela97@gmail.com>
2023-07-31 17:30:17 -0300

    Criar uma tabela para cada loja.

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ import pathlib
 emails = pd.read_excel(r"data/Emails.xlsx")
 stores = pd.read_csv(r"data/Lojas.csv", sep=";", encoding="latin-1")
 sales = pd.read_excel(r"data/Vendas.xlsx")
+
+sales = sales.merge(stores, on="ID Loja")
+
+dict_stores = dict()
+for store in stores["Loja"]:
+    dict_stores[store] = sales.loc[sales["Loja"] == store, :]
