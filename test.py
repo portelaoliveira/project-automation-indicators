74b8fb7a725be69e204618b0241722c85719bc20
Danilo Portela <daniloportela97@gmail.com>
2023-07-31 16:57:52 -0300

    Importar base de dados.

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 import pandas as pd
 import pathlib
+
+emails = pd.read_excel(r"data/Emails.xlsx")
+stores = pd.read_csv(r"data/Lojas.csv", sep=";", encoding="latin-1")
+sales = pd.read_excel(r"data/Vendas.xlsx")
