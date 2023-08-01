580ff2e63d56bb6aa767992062a019403c66e9f2
Danilo Portela <daniloportela97@gmail.com>
2023-08-01 16:14:30 -0300

    Criar arquivos de backup para cada loja.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import pathlib
+from pathlib import Path
 
 emails = pd.read_excel(r"data/Emails.xlsx")
 stores = pd.read_csv(r"data/Lojas.csv", sep=";", encoding="latin-1")
@@ -15,4 +15,21 @@ day_indicators = sales["Data"].max()
 day_indicators_formated = (
     f"{day_indicators.day}/{day_indicators.month}/{day_indicators.year}"
 )
-print(day_indicators_formated)
+
+path_backup = Path(r"Backup_Lojas")
+path_backup.mkdir(parents=True, exist_ok=True)
+
+files_folder_backup = path_backup.iterdir()
+
+list_names_backup = [files.name for files in files_folder_backup]
+
+for store in dict_stores:
+    if store not in list_names_backup:
+        store_formated = store.replace(" ", "_")
+        new_folder_stores = path_backup / store_formated
+        new_folder_stores.mkdir(parents=True, exist_ok=True)
+
+    name_file = f"{day_indicators.day}_{day_indicators.month}_{day_indicators.year}_{store_formated}.xlsx"
+    local_file = path_backup / store_formated / name_file
+
+    dict_stores[store].to_excel(local_file)
