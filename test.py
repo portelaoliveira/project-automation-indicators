1128ab2ace303e23f25fbd9d414f7dbc74bd9a03
Danilo Portela <daniloportela97@gmail.com>
2023-08-01 15:16:55 -0300

    Definir dia do indicador.

--- test.py
+++ test.py
@@ -10,3 +10,9 @@ sales = sales.merge(stores, on="ID Loja")
 dict_stores = dict()
 for store in stores["Loja"]:
     dict_stores[store] = sales.loc[sales["Loja"] == store, :]
+
+day_indicators = sales["Data"].max()
+day_indicators_formated = (
+    f"{day_indicators.day}/{day_indicators.month}/{day_indicators.year}"
+)
+print(day_indicators_formated)
